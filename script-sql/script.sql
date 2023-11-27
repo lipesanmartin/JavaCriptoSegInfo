5f350fbc66776f4b03a5d5a5212b7559c55246b4
Felipe Sanmartin <lipesanmartin@hotmail.com>
2023-11-26 21:50:18 -0300

    script sql added

---
+++ script-sql/script.sql
@@ -0,0 +1,82 @@
+CREATE
+DATABASE `seg_info` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+use
+seg_info;
+
+CREATE TABLE `medicos`
+(
+    `id`            int          NOT NULL AUTO_INCREMENT,
+    `nome`          varchar(255) NOT NULL,
+    `cpf`           varchar(11)  NOT NULL,
+    `especialidade` varchar(255) DEFAULT NULL,
+    `login`         varchar(255) NOT NULL,
+    `senha_hash`    varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `login_UNIQUE` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `pacientes`
+(
+    `id`         int          NOT NULL AUTO_INCREMENT,
+    `nome`       varchar(255) NOT NULL,
+    `cpf`        varchar(11)  NOT NULL,
+    `login`      varchar(255) NOT NULL,
+    `senha_hash` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `login_UNIQUE` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `senhas`
+(
+    `id`            int          NOT NULL,
+    `chave_secreta` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `valorespadroes`
+(
+    `id`              int         NOT NULL,
+    `descricao`       varchar(255) DEFAULT NULL,
+    `limite_superior` int         NOT NULL,
+    `limite_inferior` int         NOT NULL,
+    `unidade`         varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `vitamina_d`
+(
+    `id`    int          NOT NULL,
+    `ng_mL` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `seg_info`.`valorespadroes`
+(`id`,
+ `descricao`,
+ `limite_superior`,
+ `limite_inferior`,
+ `unidade`)
+VALUES (1,
+        'Normal',
+        100,
+        50,
+        'ng/mL'),
+       (2,
+        'Insuficiente',
+        49,
+        30,
+        'ng/mL'),
+       (3,
+        'Deficiente',
+        29,
+        20,
+        'ng/mL')
+
+
+
+
+
+
+
+
--- src/app/Main.java
+++ src/app/Main.java
@@ -17,9 +17,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        System.out.println();
 
-        System.out.println("Você é um paciente que fez um exame de vitamina D. Seu exame ficou pronto" +
-                " e você quer ver o resultado. Para isso, você precisa primeiro se cadastrar no sistema.");
+        System.out.println("""
+                Você é um paciente que fez um exame de Vitamina D.
+                Seu exame ficou pronto e você quer ver o resultado.
+                Para isso, você precisa primeiro se cadastrar no sistema.""");
+        System.out.println();
 
         // prompt de cadastro de usuario
         System.out.println("Cadastre o seu usuario:");
@@ -31,12 +35,17 @@ public class Main {
         String login = sc.nextLine();
         System.out.print("Digite a senha: ");
         String senha = sc.nextLine();
+        System.out.println();
 
         // cadastra o usuario no banco de dados
         Paciente usuario = new Paciente(nome, cpf, login, senha);
         PacienteDao pacienteDao = new PacienteDao();
         pacienteDao.adiciona(usuario);
         System.out.println("Conta criada com sucesso!");
+        System.out.println();
+        System.out.println("As informações abaixo não são do interesse do usuário comum, mas você não é um " +
+                "usuário comum, não é mesmo?");
+        System.out.println();
 
         // gera uma chave secreta para o usuario
         String userSecretKey = CryptoUtil.generateKey();
@@ -64,6 +73,7 @@ public class Main {
         VitaminaD vitaminaD = new VitaminaD(1, resultadoCriptografado);
         VitaminaDDao vitaminaDDaoInsere = new VitaminaDDao();
         vitaminaDDaoInsere.adiciona(vitaminaD);
+        System.out.println();
 
 
         System.out.println("Agora você pode logar no sistema para ver o resultado do seu exame. Você precisa" +
@@ -80,23 +90,28 @@ public class Main {
             }
         }
 
-        System.out.print("Usuario logado!");
-        System.out.println("Você clicou no botão para ver o resultado do seu exame. O sistema vai buscar o seu" +
-                " resultado no banco de dados e vai descriptografar o resultado usando a sua senha que você acabou de informar " +
-                "(mas essa última parte não é importante para um usuário comum).");
+        System.out.println();
+        System.out.println("Usuario logado!");
+        System.out.println();
+        System.out.println("""
+                Você clicou no botão para ver o resultado do seu exame.
+                O sistema vai buscar o seu resultado no banco de dados e vai
+                descriptografar o resultado usando a sua senha que você acabou de informar
+                (mais informações que um usuário comum jamais veria).""");
 
         // busca a chave secreta do usuario no banco de dados e descriptografa com a senha informada
         SenhaDao senhaDaoBusca = new SenhaDao();
         Senha chaveCriptografada = senhaDaoBusca.getSenha();
         String chaveDescriptografada = CryptoUtil.decrypt(chaveCriptografada.getKey(), senha);
         System.out.println("Chave secreta descriptografada: " + chaveDescriptografada);
+        System.out.println();
 
 
         // busca o resultado do exame no banco de dados - o id é 1 porque só tem um resultado no banco de dados
         VitaminaDDao vitaminaDDaoBusca = new VitaminaDDao();
         VitaminaD resultado = vitaminaDDaoBusca.buscaPorId(1);
         String resultadoDescriptografado = CryptoUtil.decrypt(resultado.getNgml(), chaveDescriptografada);
-        System.out.println("O resultado do seu exame é: " + resultadoDescriptografado);
+        System.out.println("O resultado do seu exame é: " + resultadoDescriptografado + " ng/mL");
         System.out.println("Valores de referência:");
         List<ValoresReferencia> valoresReferencia = new ValoresReferenciaDao().listar();
 
